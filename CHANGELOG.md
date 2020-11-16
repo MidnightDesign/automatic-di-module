67266c1bc566751913ff57b7bde31b38040e967a
Thomas Rieschl <rieschl@users.noreply.github.com>
2020-11-16 09:52:35 +0100

    Refresh (#1)
    * migrate to Laminas
    * remove unnecessary dependencies
    * remove laminas-servicemanager v2 compatibility
    * add PHP 7.4 typed properties
    * normalize composer
    * add GitHub CI

---
+++ CHANGELOG.md
@@ -0,0 +1,29 @@
+# Changelog
+
+All notable changes to this project will be documented in this file, in reverse chronological order by release.
+
+## 1.1.0 - TBD
+
+### Added
+
+- Nothing.
+
+### Changed
+
+- [#1](https://github.com/MidnightDesign/automatic-di-module/pull/1) migrates to Laminas
+
+- [#1](https://github.com/MidnightDesign/automatic-di-module/pull/1) changes minimum PHP version to 7.4
+
+### Deprecated
+
+- Nothing.
+
+### Removed
+
+- [#1](https://github.com/MidnightDesign/automatic-di-module/pull/1) removes dependency on laminas-modulemanager
+
+- [#1](https://github.com/MidnightDesign/automatic-di-module/pull/1) drops compatibility with laminas-servicemanager version 2
+
+### Fixed
+
+- Nothing.
--- tests/FunctionalTest.php
+++ tests/FunctionalTest.php
@@ -1,8 +1,11 @@
-<?php declare(strict_types = 1);
+<?php
+
+declare(strict_types=1);
 
 namespace MidnightTest\AutomaticDiModule;
 
 use Interop\Container\ContainerInterface;
+use Laminas\ServiceManager\ServiceManager;
 use Midnight\AutomaticDiModule\Module;
 use MidnightTest\AutomaticDiModule\Asset\AlsoDependsOnFooInterface;
 use MidnightTest\AutomaticDiModule\Asset\AlternateFoo;
@@ -12,83 +15,84 @@ use MidnightTest\AutomaticDiModule\Asset\Foo;
 use MidnightTest\AutomaticDiModule\Asset\FooInterface;
 use MidnightTest\AutomaticDiModule\Asset\NoDependencies;
 use PHPUnit\Framework\TestCase;
-use Zend\ServiceManager\Config;
-use Zend\ServiceManager\ServiceManager;
+
+use function array_merge;
 
 class FunctionalTest extends TestCase
 {
-    const DI = [
+    private const DI = [
         'preferences' => [
-            FooInterface::class => Foo::class
+            FooInterface::class => Foo::class,
         ],
         'classes' => [
             AlsoDependsOnFooInterface::class => [
-                'foo' => AlternateFoo::class
-            ]
-        ]
+                'foo' => AlternateFoo::class,
+            ],
+        ],
     ];
-    /** @var ContainerInterface */
-    private $container;
+    private ContainerInterface $container;
 
-    protected function setUp()
+    protected function setUp(): void
     {
         parent::setUp();
 
         $this->container = $this->createServiceManager($this->createConfig());
     }
 
-    public function testGetClassWithoutDependencies()
+    public function testGetClassWithoutDependencies(): void
     {
         $service = $this->container->get(NoDependencies::class);
 
-        $this->assertInstanceOf(NoDependencies::class, $service);
+        self::assertInstanceOf(NoDependencies::class, $service);
     }
 
-    public function testInjectInterface()
+    public function testInjectInterface(): void
     {
         $service = $this->container->get(DependsOnFooInterface::class);
 
-        $this->assertInstanceOf(DependsOnFooInterface::class, $service);
-        $this->assertInstanceOf(Foo::class, $service->getFoo());
+        self::assertInstanceOf(DependsOnFooInterface::class, $service);
+        self::assertInstanceOf(Foo::class, $service->getFoo());
     }
 
-    public function testInjectClass()
+    public function testInjectClass(): void
     {
         $service = $this->container->get(DependsOnFoo::class);
 
-        $this->assertInstanceOf(DependsOnFoo::class, $service);
-        $this->assertInstanceOf(Foo::class, $service->getFoo());
+        self::assertInstanceOf(DependsOnFoo::class, $service);
+        self::assertInstanceOf(Foo::class, $service->getFoo());
     }
 
-    public function testGetInterface()
+    public function testGetInterface(): void
     {
         $service = $this->container->get(FooInterface::class);
 
-        $this->assertInstanceOf(Foo::class, $service);
+        self::assertInstanceOf(Foo::class, $service);
     }
 
-    public function testInjectByParameter()
+    public function testInjectByParameter(): void
     {
         $service = $this->container->get(AlsoDependsOnFooInterface::class);
 
-        $this->assertInstanceOf(AlsoDependsOnFooInterface::class, $service);
-        $this->assertInstanceOf(AlternateFoo::class, $service->getFoo());
+        self::assertInstanceOf(AlsoDependsOnFooInterface::class, $service);
+        self::assertInstanceOf(AlternateFoo::class, $service->getFoo());
     }
 
+    /**
+     * @param array<string, mixed> $config
+     */
     protected function createServiceManager(array $config): ServiceManager
     {
-        try {
-            $container = new ServiceManager(new Config($config['service_manager']));
-        } catch (\TypeError $e) {
-            $container = new ServiceManager($config['service_manager']);
-        }
+        $container = new ServiceManager($config['service_manager']);
         $container->setService('Config', $config);
         return $container;
     }
 
+    /**
+     * @return array<string, mixed>
+     */
     private function createConfig(): array
     {
-        $module = new Module;
+        $module = new Module();
         $config = $module->getConfig();
         $config = array_merge($config, ['di' => self::DI]);
         return $config;
