17c3a189cc505044d1cd330ff00eb146cccd4f04
Rudolph Gottesheim <r.gottesheim@midnight-design.at>
2022-10-11 11:34:09 +0200

    Update src/AutomaticDiConfigFactory.php

--- src/AutomaticDiConfigFactory.php
+++ src/AutomaticDiConfigFactory.php
@@ -12,7 +12,8 @@ class AutomaticDiConfigFactory
     public function __invoke(ContainerInterface $container): AutomaticDiConfig
     {
         /**
-         * @var array{di: array{
+         * @var array{
+         *     di: array{
          *         preferences: array<class-string, class-string>,
          *         classes: array<class-string, array<string, class-string>>
          *     }
